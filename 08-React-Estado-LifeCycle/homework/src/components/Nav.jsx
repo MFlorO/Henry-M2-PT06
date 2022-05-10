4ea901ec4ce6cef5bce6f63ec5759d33b1119001
Flor oldani <floroldani@hotmail.com>
2022-05-10 00:49:21 -0300

    HomeworkEstados

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -3,9 +3,19 @@ import Logo from '../logoHenry.png'
 import SearchBar from './SearchBar.jsx';
 import './Nav.css';
 
+
 function Nav({onSearch}) {
   return (
-    <div>Nav</div>
+    <div className="Nav">
+    
+    <img className="img" src={Logo} alt="LogoHenry" />
+    {/* En src se puede poner {logo} y no toda la ruta de la ubicacion del archivo porque se importo creando una variable */}
+    <span>Henry - Weather App</span>
+    < SearchBar className="searchBar" onSearch={onSearch}/>
+    {/* Una vez pasada a "Nav" tengo que asociarle la funcion a "SearchBar" */}
+
+    </div>
+
   );
 };
 
--- 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/SearchBar.jsx
@@ -1,16 +1,37 @@
 import React, { useState } from "react";
+import "./SearchBar.css";
 
 export default function SearchBar({onSearch}) {
+
+  const [city, setcity] = useState("");
+
   return (
-    <form onSubmit={(e) => {
+    <form onSubmit={(e) => {  // "onSubmit" significa cuando el formulario se envie
       e.preventDefault();
-      onSearch("Cairns");
+      onSearch(city);
+      setcity("");
+      
     }}>
-      <input
+      <input //Lugar donde escriba el usuario
         type="text"
         placeholder="Ciudad..."
+
+
+        //El objetivo es que cuando el usuario escriba en el input modifique mi estado
+        //Despues ejecuto mi funcion "onsearch" pasando mi estado que fue lo que el usuario escribio
+        value={city} //El valor del imput va a ser igual al estado city
+
+        //onChange es un evento que detecta si el usuario escribe o borra. 
+        //Este ejecuta una funcion. Y recibe por efecto un estado:"e"
+        //Quiero que este modifique mi estado.. por esto, llamo a "setcity".
+        //Quiero que modifique a setCity con el valor que el usuario escriba: con el evento accede al target
+        //TARGET ES EL ELEMENTO QUE DISPARO ESE EVENTO --> Estamos parados ya en el input
+        //Por ultimo quiero acceder al valor del input
+
+        onChange={e => setcity(e.target.value)}
+
       />
-      <input type="submit" value="Agregar" />
+      <input className="input" type="submit" value="Agregar" />
     </form>
   );
 }
