e2c908e61bafd5faaf0ea3bc663648a28e013707
Flor oldani <floroldani@hotmail.com>
2022-05-11 00:36:50 -0300

    HomeworkFinal

--- 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
+++ 08-React-Estado-LifeCycle/homework/src/components/Nav.jsx
@@ -6,15 +6,17 @@ import './Nav.css';
 
 function Nav({onSearch}) {
   return (
-    <div className="Nav">
+    <nav className="Nav">
     
     <img className="img" src={Logo} alt="LogoHenry" />
     {/* En src se puede poner {logo} y no toda la ruta de la ubicacion del archivo porque se importo creando una variable */}
+    
     <span>Henry - Weather App</span>
+    
     < SearchBar className="searchBar" onSearch={onSearch}/>
     {/* Una vez pasada a "Nav" tengo que asociarle la funcion a "SearchBar" */}
 
-    </div>
+    </nav>
 
   );
 };
