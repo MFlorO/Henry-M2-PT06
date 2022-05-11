efa7cbb1015fa03774c56fb0413a5c9f86e3a69b
Flor oldani <floroldani@hotmail.com>
2022-05-11 00:35:49 -0300

    HomeworkRouter

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+export default function About () {
+ 
+    return (
+      <>
+      <h3> About </h3>
+      <p> Esta apliacion muestra el clima de distintas ciudades del mundo</p> 
+      </>
+    )
+  
+}
+
--- 09-React-Routing/homework/src/components/Cards.jsx
+++ 09-React-Routing/homework/src/components/Cards.jsx
@@ -3,10 +3,13 @@ import './Cards.css';
 
 import Card from './Card.jsx';
 
+
 export default function Cards({cities, onClose}) {
   return (
+
     <div className='cards'>
       {cities.map(c => <Card
+          id={c.id}
           key={c.id}
           max={c.max}
           min={c.min}
@@ -15,5 +18,7 @@ export default function Cards({cities, onClose}) {
           onClose={() => onClose(c.id)}
         /> )}
     </div>
+ 
+
   );
 }
